bf5717150f49db42703c8cad9b3a1887d57254c9
Sergey Yakupov <motherboard98@yandex.ru>
2023-12-04 19:21:16 +0300

    setup

---
+++ smtuIdle/initialize_db.py
@@ -0,0 +1,22 @@
+from peewee import SqliteDatabase
+from models import Purchase, User, Role, UserRole  # Замените models на имя вашего модуля или файла с определением моделей
+import os
+
+db = SqliteDatabase('test.db')  # Замените на имя вашей базы данных
+
+def initialize_database():
+    # Проверяем, существует ли файл маркера
+    if not os.path.exists('database_initialized.marker'):
+        db.connect()
+        db.create_tables([Purchase, User, Role, UserRole])
+        admin_user = User.create(username='admin', password='sa')
+        regular_user = User.create(username='user', password='sa')
+        admin_role = Role.create(name='Администратор')
+        user_role = Role.create(name='Пользователь')
+        UserRole.create(user=regular_user, role=user_role)
+        UserRole.create(user=admin_user, role=admin_role)
+        db.close()
+
+        # Создаем файл маркера, чтобы показать, что инициализация была завершена
+        with open('database_initialized.marker', 'w'):
+            pass
