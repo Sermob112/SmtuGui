7d8b4057eb032db601c853fe54d05a878e910b7a
Sergey Yakupov <motherboard98@yandex.ru>
2023-12-31 14:26:22 +0300

    Улучшено интерфейс

--- smtuIdle/DBtest.py
+++ smtuIdle/DBtest.py
@@ -1,8 +1,9 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget,QPushButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget,QPushButton,QHeaderView
 from peewee import SqliteDatabase, Model, AutoField, CharField, IntegerField, FloatField, DateField
 from playhouse.shortcuts import model_to_dict
 from datetime import date
 from models import Purchase
+from PySide6.QtWidgets import QSizePolicy
 # Код вашей модели остается таким же, как вы предоставили в предыдущем сообщении.
 
 # Создаем соединение с базой данных
@@ -10,16 +11,17 @@ db = SqliteDatabase('test.db')
 
 
 
-# Создаем виджет для отображения данных
+
 class PurchasesWidget(QWidget):
     def __init__(self):
         super().__init__()
 
         # Создаем таблицу для отображения данных
         self.table = QTableWidget(self)
-        self.table.setColumnCount(3)  # Установите количество столбцов по своему усмотрению
-        self.table.setHorizontalHeaderLabels(["Идентификатор", "Номер заказа", "Предмет аукциона"])
-
+        self.table.setColumnCount(2)  # Установите количество столбцов по своему усмотрению
+        # self.table.setHorizontalHeaderLabels(["Идентификатор", "Номер заказа", "Предмет аукциона"])
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         # Создаем кнопки для навигации
         self.prev_button = QPushButton("Назад", self)
         self.next_button = QPushButton("Вперед", self)
@@ -27,7 +29,10 @@ class PurchasesWidget(QWidget):
         # Устанавливаем обработчики событий для кнопок
         self.prev_button.clicked.connect(self.show_previous)
         self.next_button.clicked.connect(self.show_next)
-
+        # Скрыть номера строк и столбцов
+        self.table.setShowGrid(True)
+        self.table.verticalHeader().setVisible(False)
+        self.table.horizontalHeader().setVisible(False)
         # Текущая позиция записи
         self.current_position = 0
 
@@ -49,12 +54,53 @@ class PurchasesWidget(QWidget):
         # Получаем текущую запись
         current_purchase = self.purchases_list[self.current_position]
 
-        # Заполняем таблицу данными из текущей записи
+        # Добавляем данные в виде "название поля - значение поля"
+        self.add_row_to_table("Идентификатор", str(current_purchase.Id))
+        self.add_row_to_table("Закон", current_purchase.PurchaseOrder)
+        self.add_row_to_table("Реестровый номер", current_purchase.RegistryNumber)
+        self.add_row_to_table("Метод закупки", current_purchase.ProcurementMethod)
+        self.add_row_to_table("Наименование закупки", current_purchase.PurchaseName)
+        self.add_row_to_table("Предмет аукциона", current_purchase.AuctionSubject)
+        self.add_row_to_table("Код идентификации закупки", current_purchase.PurchaseIdentificationCode)
+        self.add_row_to_table("Номер лота", str(current_purchase.LotNumber))
+        self.add_row_to_table("Наименование лота", current_purchase.LotName)
+        self.add_row_to_table("Начальная максимальная цена контракта", str(current_purchase.InitialMaxContractPrice))
+        self.add_row_to_table("Валюта", current_purchase.Currency)
+        self.add_row_to_table("Начальная максимальная цена контракта в валюте", str(current_purchase.InitialMaxContractPriceInCurrency))
+        self.add_row_to_table("Валюта контракта", current_purchase.ContractCurrency)
+        self.add_row_to_table("Классификация ОКДП", current_purchase.OKDPClassification)
+        self.add_row_to_table("Классификация ОКПД", current_purchase.OKPDClassification)
+        self.add_row_to_table("Классификация ОКПД2", current_purchase.OKPD2Classification)
+        self.add_row_to_table("Код позиции", current_purchase.PositionCode)
+        self.add_row_to_table("Наименование заказчика", current_purchase.CustomerName)
+        self.add_row_to_table("Организация закупки", current_purchase.ProcurementOrganization)
+        self.add_row_to_table("Дата размещения", str(current_purchase.PlacementDate))
+        self.add_row_to_table("Дата обновления", str(current_purchase.UpdateDate))
+        self.add_row_to_table("Этап закупки", current_purchase.ProcurementStage)
+        self.add_row_to_table("Особенности закупки", current_purchase.ProcurementFeatures)
+        self.add_row_to_table("Дата начала заявки", str(current_purchase.ApplicationStartDate))
+        self.add_row_to_table("Дата окончания заявки", str(current_purchase.ApplicationEndDate))
+        self.add_row_to_table("Дата аукциона", str(current_purchase.AuctionDate))
+        self.add_row_to_table("Количество запросов", str(current_purchase.QueryCount))
+        self.add_row_to_table("Количество ответов", str(current_purchase.ResponseCount))
+        self.add_row_to_table("Среднее значение цены", str(current_purchase.AveragePrice))
+        self.add_row_to_table("Минимальная цена", str(current_purchase.MinPrice))
+        self.add_row_to_table("Максимальная цена", str(current_purchase.MaxPrice))
+        self.add_row_to_table("Среднее квадратичное отклонение", str(current_purchase.StandardDeviation))
+        self.add_row_to_table("Коэффициент вариации", str(current_purchase.CoefficientOfVariation))
+        self.add_row_to_table("НМЦК рыночная", str(current_purchase.NMCKMarket))
+        self.add_row_to_table("Лимит финансирования", str(current_purchase.FinancingLimit))
+       
+
+    def add_row_to_table(self, label_text, value_text):
         row_position = self.table.rowCount()
         self.table.insertRow(row_position)
-        self.table.setItem(row_position, 0, QTableWidgetItem(str(current_purchase.Id)))
-        self.table.setItem(row_position, 1, QTableWidgetItem(current_purchase.PurchaseOrder))
-        self.table.setItem(row_position, 2, QTableWidgetItem(current_purchase.AuctionSubject))
+
+        label_item = QTableWidgetItem(label_text)
+        value_item = QTableWidgetItem(value_text)
+
+        self.table.setItem(row_position, 0, label_item)
+        self.table.setItem(row_position, 1, value_item)
 
     def show_previous(self):
         if self.current_position > 0:
@@ -65,7 +111,7 @@ class PurchasesWidget(QWidget):
         if self.current_position < len(self.purchases_list) - 1:
             self.current_position += 1
             self.show_current_purchase()
-
+    
 
 # Создаем экземпляр виджета
 # widget = PurchasesWidget()
