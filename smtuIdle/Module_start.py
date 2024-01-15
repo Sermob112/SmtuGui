76e6f8bdfab7c23476ffb9bfabf4df3ceb56317e
Sergey Yakupov <motherboard98@yandex.ru>
2024-01-15 16:34:12 +0300

    Добавлена кнопка выхода

---
+++ smtuIdle/Module_start.py
@@ -0,0 +1,19 @@
+# from PySide6.QtWidgets import QMainWindow, QApplication, QWidget, QFormLayout, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
+# from auth import *
+# from PySide6.QtWidgets import *
+# from initialize_db import initialize_database
+# from MainWindow import Ui_MainWindow
+# from PySide6.QtCore import Qt
+
+# from authWindow import AuthWindow
+
+# class AuthManager:
+#     def open_auth_window(self):
+#             app = QApplication([])
+#             auth_window = AuthWindow()
+#             auth_window.show()
+#             app.exec()
+
+
+# vart = AuthManager()
+# vart.open_auth_window()
