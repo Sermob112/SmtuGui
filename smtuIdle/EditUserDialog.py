2dfb1a93770f5ae7ea04dc1da03fe39eaaf766d7
Sergey Yakupov <motherboard98@yandex.ru>
2024-02-14 18:29:17 +0300

    Закотовка с ролями

---
+++ smtuIdle/EditUserDialog.py
@@ -0,0 +1,73 @@
+from PySide6.QtWidgets import *
+from models import UserRole, Role,User
+
+class EditUserDialog(QDialog):
+    def __init__(self, user_id, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Редактирование пользователя')
+        self.user_id = user_id
+
+        layout = QVBoxLayout(self)
+
+        label1 = QLabel("Выбирите роль")
+        self.role_combo = QComboBox()
+        self.populate_roles()
+        layout.addWidget(label1)
+        layout.addWidget(self.role_combo)
+
+        label2 = QLabel("Введите имя пользователя")
+        self.username_edit = QLineEdit()
+        layout.addWidget(label2)
+        layout.addWidget(self.username_edit)
+
+        label3 = QLabel("Введите пароль")
+        self.upassword_edit = QLineEdit()
+        layout.addWidget(label3)
+        layout.addWidget(self.upassword_edit)
+
+        self.edit_button = QPushButton('Редактировать')
+        self.edit_button.clicked.connect(self.edit_user)
+        layout.addWidget(self.edit_button)
+
+        # Получите данные о пользователе и заполните поля
+        self.populate_user_data()
+
+    def populate_roles(self):
+        roles = Role.select()
+        for role in roles:
+            self.role_combo.addItem(role.name, role.id)
+
+    def populate_user_data(self):
+        # Получите данные о пользователе и заполните поля
+        user = User.get(User.id == self.user_id)
+        self.username_edit.setText(user.username)
+        # Дополнительно заполните поле пароля, если это необходимо
+
+        # Выберите роль пользователя в выпадающем списке
+        user_role = UserRole.get(UserRole.user == user)
+        role_index = self.role_combo.findData(user_role.role.id)
+        if role_index != -1:
+            self.role_combo.setCurrentIndex(role_index)
+
+    def edit_user(self):
+        selected_role_id = self.role_combo.currentData()
+        selected_role = Role.get(Role.id == selected_role_id)
+        username = self.username_edit.text()
+        password = self.upassword_edit.text()
+
+        if username:
+            # Обновите пользователя
+            user = User.get(User.id == self.user_id)
+            user.username = username
+            # Дополнительно обновите пароль, если это необходимо
+            user.save()
+
+            # Обновите роль пользователя
+            UserRole.update(role=selected_role).where(UserRole.user == user).execute()
+            self.accept()
+        else:
+            QMessageBox.warning(self, 'Внимание', 'Введите имя пользователя.')
+
+
+# Где-то в вашем коде, вероятно в методе add_user_dialog, создайте и отобразите диалог:
+
--- smtuIdle/debugWindow.py
+++ smtuIdle/debugWindow.py
@@ -1,10 +1,14 @@
-from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6 import *
 from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import QColor
 import statistics
 from models import *
 from peewee import *
 import pandas as pd
 from parserV3 import *
+from AddUserDialog import AddUserDialog
+from EditUserDialog import EditUserDialog
 import sys
 
 class DebugWidget(QWidget):
@@ -12,30 +16,95 @@ class DebugWidget(QWidget):
         super().__init__()
 
         self.init_ui()
-
+        self.buttons_added = False 
     def init_ui(self):
         layout = QVBoxLayout()
+        
         lbl_debug = QLabel('Отладка', self)
         layout.addWidget(lbl_debug)
-        # Добавляем таблицу
-        self.table_widget = QTableWidget()
-        self.table_widget.setColumnCount(3)  # Количество столбцов
-        self.table_widget.setHorizontalHeaderLabels(['Имя пользователя', 'Вход', 'Выход'])  # Заголовки столбцов
-        layout.addWidget(self.table_widget)
-        self.load_logs()
-    # Создаем кнопку
-        btn_load_csv = QPushButton('Загрузить CSV файл', self)
-        btn_load_csv.clicked.connect(self.show_file_dialog)
-        layout.addWidget(btn_load_csv)
 
-        btn_delete_data = QPushButton('Удалить все данные', self)
-        btn_delete_data.clicked.connect(self.delete_all_data)
-        layout.addWidget(btn_delete_data)
-        # Добавляем кнопку на виджет
-        
+        # Создаем компонент вкладок
+        tab_widget = QTabWidget()
+
+        # Добавляем вкладки
+        tab_widget.addTab(self.create_logs_tab(), 'Логи')
+        tab_widget.addTab(self.create_users_tab(), 'Пользователи и роли')
+        tab_widget.addTab(self.bd_contoll(), 'Управление БД')
+
+        layout.addWidget(tab_widget)
 
         self.setLayout(layout)
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+    def create_logs_tab(self):
+        # Создаем вкладку для первых данных
+        tab = QWidget()
+        layout = QVBoxLayout(tab)
+
+        # Таблица для первых данных
+        self.table_widget = QTableWidget()
+        self.table_widget.setColumnCount(3)
+        self.table_widget.setHorizontalHeaderLabels(['Имя пользователя', 'Вход', 'Выход'])
+        layout.addWidget(self.table_widget)
+        self.table_widget.horizontalHeader().setStretchLastSection(True)
+        self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        
+        # Загрузка первых данных
+        self.load_logs()
+        return tab
+
+    def create_users_tab(self):
+        
+        # Создаем вкладку для данных пользователей
+        tab = QWidget()
+        self.layout_user = QVBoxLayout(tab)
+
+        # Таблица для данных пользователей
+        self.users_table_widget = QTableWidget()
+        self.users_table_widget.setColumnCount(3)  # Количество столбцов
+        self.users_table_widget.setHorizontalHeaderLabels(['№','Пользователь', 'Роль'])  # Заголовки столбцов
+        self.layout_user.addWidget(self.users_table_widget)
+        self.users_table_widget.horizontalHeader().setStretchLastSection(True)
+        self.users_table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.users_table_widget.verticalHeader().setVisible(False)
+        self.users_table_widget.horizontalHeader().setVisible(True)
+        
+        # Загрузка данных пользователей
+        self.load_user_roles()
+         # Кнопка "Добавить пользователя"
+        btn_add_user = QPushButton('Добавить пользователя', self)
+        btn_add_user.setFixedWidth(300)  # Устанавливаем минимальную ширину кнопки
+        self.layout_user.addWidget(btn_add_user, alignment=Qt.AlignCenter)
+         # Привязываем обработчик события clicked
+        btn_add_user.clicked.connect(self.add_user_dialog)
+        self.users_table_widget.itemSelectionChanged.connect(self.user_selection_changed)
+
+        # # Кнопка "Редактировать пользователя"
+        # btn_edit_user = QPushButton('Редактировать пользователя', self)
+        # btn_edit_user.setFixedWidth(300)  # Устанавливаем минимальную ширину кнопки
+        # layout.addWidget(btn_edit_user, alignment=Qt.AlignCenter)
+        # btn_edit_user.clicked.connect(self.edit_user_dialog)
+        # # Кнопка "Удалить пользователя"
+        # btn_delete_user = QPushButton('Удалить пользователя', self)
+        # btn_delete_user.setFixedWidth(300)  # Устанавливаем минимальную ширину кнопки
+        # layout.addWidget(btn_delete_user, alignment=Qt.AlignCenter)
+
+        return tab
+    def bd_contoll(self):
+        tab = QWidget()
+        layout = QVBoxLayout(tab)
+        buttonLAyout = QHBoxLayout(tab)
+        # Кнопка загрузки CSV файла
+        btn_load_csv = QPushButton('Загрузить CSV файл отладки', self)
+        btn_load_csv.setFixedWidth(300)  # Устанавливаем минимальную ширину кнопки
+        buttonLAyout.addWidget(btn_load_csv, alignment=Qt.AlignTop)  # Устанавливаем выравнивание кнопки вверх
+
+        # Кнопка удаления всех данных
+        btn_delete_data = QPushButton('Удалить все данные БД', self)
+        btn_delete_data.setFixedWidth(300)  # Устанавливаем минимальную ширину кнопки
+        buttonLAyout.addWidget(btn_delete_data, alignment=Qt.AlignTop)  # Устанавливаем выравнивание кнопки вверх
+        layout.addLayout(buttonLAyout)
+        return tab
 
     def show_file_dialog(self):
         file_dialog = QFileDialog(self)
@@ -53,7 +122,18 @@ class DebugWidget(QWidget):
             else:
                 QMessageBox.information(self,"Ошибка", "Ошибка при удалении данных")
         pass
-    
+    def load_user_roles(self):
+        # Получаем данные пользователей из модели UserRole
+        users_roles = UserRole.select()
+        # Очищаем таблицу перед загрузкой новых данных
+        self.users_table_widget.clearContents()
+        self.users_table_widget.setRowCount(0)
+        for user_role in users_roles:
+            row_position = self.users_table_widget.rowCount()
+            self.users_table_widget.insertRow(row_position)
+            self.users_table_widget.setItem(row_position, 0, QTableWidgetItem(str(user_role.user.id)))
+            self.users_table_widget.setItem(row_position, 1, QTableWidgetItem(user_role.user.username))  # Предполагая, что у пользователя есть атрибут username
+            self.users_table_widget.setItem(row_position, 2, QTableWidgetItem(user_role.role.name))  # Предполагая, что у роли есть атрибут name
     def load_logs(self):
         logs = UserLog.select()
         self.table_widget.setRowCount(len(logs))
@@ -66,8 +146,91 @@ class DebugWidget(QWidget):
             self.table_widget.setItem(idx, 0, username_item)
             self.table_widget.setItem(idx, 1, login_time_item)
             self.table_widget.setItem(idx, 2, logout_time_item)
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     csv_loader_widget = DebugWidget()
-#     csv_loader_widget.show()
-#     sys.exit(app.exec_())
+
+
+    def edit_user(self):
+        # Получаем данные пользователя и его текущую роль
+        selected_user_id = self.user_combo.currentData()
+        selected_user = User.get(User.id == selected_user_id)
+        selected_role_id = self.role_combo.currentData()
+        selected_role = Role.get(Role.id == selected_role_id)
+
+        # Проверяем, что пользователь и роль выбраны
+        if selected_user and selected_role:
+            # Обновляем роль пользователя
+            UserRole.update(role=selected_role).where(UserRole.user == selected_user).execute()
+            self.accept()
+        else:
+            QMessageBox.warning(self, 'Внимание', 'Выберите пользователя и роль.')
+
+
+    def user_selection_changed(self):
+        selected_items = self.users_table_widget.selectedItems()
+        if selected_items:
+            # Очищаем предыдущее выделение
+            for row in range(self.users_table_widget.rowCount()):
+                for col in range(self.users_table_widget.columnCount()):
+                    item = self.users_table_widget.item(row, col)
+                    
+
+            # Подсвечиваем выбранную строку
+            for item in selected_items:
+                item.setBackground(QColor("lightblue"))
+
+            # Добавляем кнопки только к выбранной строке
+            selected_row = selected_items[0].row() if selected_items else -1
+            if selected_row != -1:
+                self.add_buttons_to_user_row(selected_row)
+                
+    def add_buttons_to_user_row(self, row):
+        if not self.buttons_added:
+            # Кнопка "Редактировать пользователя"
+            btn_edit_user = QPushButton('Редактировать пользователя', self)
+            btn_edit_user.setFixedWidth(300)  # Устанавливаем минимальную ширину кнопки
+            self.layout_user.addWidget(btn_edit_user, alignment=Qt.AlignCenter)
+            btn_edit_user.clicked.connect(self.edit_user_dialog)
+            # Кнопка "Удалить пользователя"
+            btn_delete_user = QPushButton('Удалить пользователя', self)
+            btn_delete_user.setFixedWidth(300)  # Устанавливаем минимальную ширину кнопки
+            self.layout_user.addWidget(btn_delete_user, alignment=Qt.AlignCenter)
+            btn_delete_user.clicked.connect(self.delete_user)
+            self.buttons_added = True
+
+    def add_user_dialog(self):
+        dialog = AddUserDialog(self)
+        if dialog.exec() == QDialog.Accepted:
+            self.load_user_roles()
+
+    def edit_user_dialog(self):
+        dialog = EditUserDialog(self,user_id = 1)
+        user_id = 1
+        if dialog.exec() == QDialog.Accepted:
+            self.load_user_roles()
+
+    def delete_user(self):
+        selected_items = self.users_table_widget.selectedItems()
+        if selected_items:
+            # Получаем ID пользователя из первого столбца выбранной строки
+            selected_row = selected_items[0].row()
+            user_id_item = self.users_table_widget.item(selected_row, 0)
+            user_id = int(user_id_item.text())  # Преобразуем текст в целочисленный ID пользователя
+            print(user_id)
+            # Здесь вы должны выполнить удаление пользователя с использованием полученного user_id
+            # Например, если вы используете Peewee ORM, это может выглядеть так:
+            try:
+                user = User.get(User.id == user_id)
+                user.delete_instance()  # Удаляем пользователя из базы данных
+                # Обновляем отображение таблицы
+                self.load_user_roles()
+                QMessageBox.information(self, "Успешно", "Пользователь успешно удален.")
+            except User.DoesNotExist:
+                QMessageBox.warning(self, "Ошибка", "Пользователь с указанным ID не найден.")
+        else:
+            QMessageBox.warning(self, "Ошибка", "Пользователь не выбран для удаления.")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    csv_loader_widget = DebugWidget()
+    csv_loader_widget.show()
+    sys.exit(app.exec_())
+            
