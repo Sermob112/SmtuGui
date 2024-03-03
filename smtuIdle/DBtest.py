d0a72d69afbabdb02d64bbbc9740745c4cb2491a
Sergey Yakupov <motherboard98@yandex.ru>
2024-03-03 16:51:28 +0300

    Добавлен голубой цвет как будто сслыка

--- smtuIdle/DBtest.py
+++ smtuIdle/DBtest.py
@@ -372,6 +372,8 @@ class PurchasesWidget(QWidget):
                 value_item.setBackground(QColor(200, 255, 200))  # Светло-зеленый
         if label_text == 'Реестровый номер':
             value_item.setData(Qt.UserRole, f'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString={self.purchases_list[self.current_position].RegistryNumber}&morphology=on&search-filter=Дате+размещения&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&ca=on&pc=on&pa=on&currencyIdGeneral=-1')
+            value_item.setForeground(Qt.blue)  # Голубой цвет текста
+        
         self.table.setItem(row_position, 0, label_item)
         self.table.setItem(row_position, 1, value_item)
         
